c15c23a3075a8ee51f06608ce1788b8b55a15aac
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-03-21 20:36:13 -0400

    Test other server

--- httpRequest.c
+++ httpRequest.c
@@ -21,7 +21,7 @@ int main(int argc,char *argv[])
 
     /* first what are we going to send and where are we going to send it? */
     int portno =        80;
-    char *host =        "google.com"//"cs590.herokuapp.com";
+    char *host =        "google.com";//"cs590.herokuapp.com";
     char *message_fmt = "GET /%s HTTP/1.0\r\n\r\n";
 
     struct hostent *server;
